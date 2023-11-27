38171dc6e008cece939c937a2cc12a5d51842bd0
yadanza <mattyadanza1@gmail.com>
2023-11-27 11:56:22 -0600

    exercise 3 resverseString completed 2 ways: 1. slice method 2.substr method

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+let reversed = ""
 
+const reverseString = function(string) {
+for(let i = 1; i < string.length + 1; i++){
+    reversed += string.substr(-i, 1)
+}
+
+return reversed
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
+ 
