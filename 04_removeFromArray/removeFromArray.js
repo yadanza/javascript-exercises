6e3d70b346a11cacca6b289dbd9dbbb1979c0b34
yadanza <mattyadanza1@gmail.com>
2023-11-27 12:27:56 -0600

    completed removeFromArray with indexOf and concat

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
-
+const filteredArray = []
+const removeFromArray = function(arr,num) {
+let removeIndex = arr.indexOf(num)
+return filteredArray.concat(arr.slice(0, removeIndex).concat(arr.slice(removeIndex + 1)))
 };
+
+console.log(removeFromArray([1,2,3,4,5,6], 4))
 
 // Do not edit below this line
 module.exports = removeFromArray;
