2adcdf09fcfea4bc0204b2925cb9b5dc78c4a3e6
yadanza <mattyadanza1@gmail.com>
2023-11-27 10:36:31 -0600

    Completed the second exercise 02_repeatString.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+let repeatedString = ""
+const repeatString = function(string, num) {
+for(let i = 0; i < num; i++){
+    repeatedString += string
+}
+return repeatedString
 };
 
 // Do not edit below this line
